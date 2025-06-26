b541fdfcb7a6797d6b49c46f43664584d52119d5
Duvan0417 <narvaezduvan29@gmail.com>
2025-06-25 20:28:46 -0500

    correciones

--- app/Models/Revision.php
+++ app/Models/Revision.php
@@ -9,6 +9,9 @@ use App\Models\Coche;
 class Revision extends Model
 {
     use HasFactory;
+       //darwin deber tener en cuenta que las migraciones se declaran en ingles
+       //si no por el contrario debes especificar la tablas que vas llamar como en la siguiente linea 
+       protected $table = 'revisiones';
 
     protected $fillable = [ 'filtro', 'aceite', 'frenos', 'coche_id'];
 
